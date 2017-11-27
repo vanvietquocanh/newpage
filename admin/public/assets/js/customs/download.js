7694d19ba37048fbf30ea4bcb128fb46e68480ca
master <vanvietquocanhanh@hotmail.com>
2017-11-27 22:27:14 +0700

    ads

--- admin/public/assets/js/customs/download.js
+++ admin/public/assets/js/customs/download.js
@@ -4,32 +4,19 @@ var sortOS = $("#os");
 var sortCountry = $("#country");
 var OSdownload = $("");
 var nameDownload = $("");
+var tagDownload = $("#download")
 var countryDownload = $("");
 var urlDownload = $("");
 SortItems.prototype.download = function(filename){
 	var text = "";
     $.each(this.list,function(index, el) {
     	console.log(el)
-        text+= `\r\n ${el}`;
+        text+= `\r\n${el.url}|${(el.country[0])?el.country[0].country_code:""}|${el.name}|${el.id}|${el.platform.split(".png")[0].split("/images/")[1]}`;
     });
     var blob = new Blob([text],{type:"octet/stream"});
     var url  = window.URL.createObjectURL(blob);
-    download.attr("href",url);
-    download.download = filename;
-};
-SortItems.prototype.sortcountry = function(imgIconOS,country,date,result){
-	if(imgIconOS === "all"){
-		let countryCode = $(".sel-items").children().prevObject;
-		sortItems.itemsRender(country, countryCode, "countryCode", "sel-items");
-	}else{
-		if(country!=="ALL"){
-			let countryCode = $(".platform");
-			sortItems.itemsRender(country, countryCode, "countryCode", "platform");
-		}else{
-			$(".activeItems").removeClass('activeItems multi2');
-			$(".platform").fadeIn('slow');
-		}
-	}
+    tagDownload.attr("href",url);
+    tagDownload.download = filename;
 };
 SortItems.prototype.dateTime = function (value) {
 	switch (value) {
@@ -47,32 +34,47 @@ SortItems.prototype.dateTime = function (value) {
 			break;
 	}
 }
+SortItems.prototype.sortcountry = function(imgIconOS,country,date,result){
+	if(imgIconOS === "all"){
+		let countryCode = $(".sel-items").children().prevObject;
+		sortItems.itemsRender(country, countryCode, "countryCode", "sel-items");
+	}else{
+		if(country!=="ALL"){
+			let countryCode = $(".sel-items");
+			countryCode.removeClass("countryCode");
+			sortItems.itemsRender(country, countryCode, "countryCode", "platform");
+		}else{
+			$(".platform").removeClass('countryCode');
+			$(".platform").fadeIn('slow');
+		}
+	}
+};
 SortItems.prototype.itemsRender = function(condition, arraylist, namefn, hide){
 	$(`.${hide}`).hide();
-	$(`sel-items`).removeClass("multi2");
+	$(`sel-items`).removeClass("countryCode");
 	$.each(arraylist, function(index, el) {
 		let items = ($($(el).children()[7]).html()===undefined)?"":$($(el).children()[7]).html();
 		if(items.indexOf(condition)!==-1){
-			$(el).addClass(`activeItems ${namefn}`);
+			$(el).addClass(`${namefn}`);
 		}else if(condition === "ALL"){
-			$(`.${hide}`).fadeIn('slow').removeClass(`activeItems ${namefn}`);
+			$(`.${hide}`).fadeIn('slow').removeClass(`${namefn}`);
 		}else{
-			$(el).removeClass(`activeItems ${namefn}`);
+			$(el).removeClass(`${namefn}`);
 		}
 	});
 	$(`.${namefn}`).fadeIn('slow');
 };
 SortItems.prototype.imgRender = function(condition, arraylist, namefn, hide){
 	$(`.${hide}`).hide();
-	$('sel-items').removeClass("multi2");
+	$('sel-items').removeClass("platform");
 	$.each(arraylist, function(index, el) {
 		let items = $($(el).children()[1]).children().attr("src");
 		if(items.indexOf(condition)!==-1){
-			$(el).addClass(`activeItems ${namefn}`);
+			$(el).addClass(`${namefn}`);
 		}else if(condition === "all"){
-			$(`.${hide}`).removeClass(`activeItems ${namefn}`).fadeIn('slow');
+			$(`.${hide}`).removeClass(`${namefn}`).fadeIn('slow');
 		}else{
-			$(el).removeClass(`activeItems ${namefn}`);
+			$(el).removeClass(`${namefn}`);
 		}
 	});
 	$(`.${namefn}`).fadeIn('slow');
@@ -84,16 +86,17 @@ SortItems.prototype.sortplatform = function(imgIconOS,country,date,result){
 	}else{
 		if(imgIconOS!=="all"){
 			let platFormImages = $(".countryCode");
-			sortItems.imgRender(imgIconOS, platFormImages, "platform", "activeItems");
+			platFormImages.removeClass("platform");
+			sortItems.imgRender(imgIconOS, platFormImages, "platform", "countryCode");
 		}
-		// else{
-		// 	$(".activeItems").removeClass('activeItems multi2');
-		// 	$(".countryCode").fadeIn('slow');
-		// }
+		else{
+			$(".countryCode").removeClass('platform');
+			$(".countryCode").fadeIn('slow');
+		}
 	}
 };
-download.click(function(event) {
-	console.log($(`.showItems`))
+tagDownload.click(function(event) {
+	sortItems.download("text.txt")
 });
 sortOS.change(function(event) {
 	let valueUserSelect = event.target.value;
--- admin/routes/singin.js
+++ admin/routes/singin.js
@@ -1,40 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	res.render("singin");
-	// var socialLogin			= new socialLoginClass({
- //    app:	app,
- //    url:	'http://localhost:3000',
- //    onAuth:	function(req, type, uniqueProperty, accessToken, refreshToken, profile, done) {
- //        	findOrCreate({
-	//             profile:	profile,       
-	//             property:	uniqueProperty,
-	//             type:		type           
- //       		}, function(user) {
- //            	done(null, user);   
- //            	console.log(user)
- //        	});
- //    	}
-	// });
-	// socialLogin.use({
- //    facebook:	{
- //        settings:	{
- //            clientID:		"148127002482488",
- //            clientSecret: 	"f85117c134e80d3ecd07b4baa3bb41a4",
- //            authParameters:	{
- //                scope: 'read_stream,manage_pages'
- //            }
- //        },
- //        url:	{
- //            auth:		"/",
- //            callback: 	"/",
- //            success:	'/index',
- //            fail:		'/'     
- //        }
- //    },
-	// });
+	firebase.initializeApp({
+	    databaseURL: 'https://learn-bfd53.firebaseio.com/',
+	    serviceAccount: 'learn-ce91a4e1f074.json',
+	});
+	firebase.database().ref('/').set({
+	    username: "kubonne",
+	    email: "kubonne@gmail.com"
+	});
+	var socialLogin			= new socialLoginClass({
+    app:	app,
+    url:	'http://localhost:3000',
+    onAuth:	function(req, type, uniqueProperty, accessToken, refreshToken, profile, done) {
+        	findOrCreate({
+	            profile:	profile,       
+	            property:	uniqueProperty,
+	            type:		type           
+       		}, function(user) {
+            	done(null, user);   
+            	console.log(user)
+        	});
+    	}
+	});
+	socialLogin.use({
+    facebook:	{
+        settings:	{
+            clientID:		"148127002482488",
+            clientSecret: 	"f85117c134e80d3ecd07b4baa3bb41a4",
+            authParameters:	{
+                scope: 'read_stream,manage_pages'
+            }
+        },
+        url:	{
+            auth:		"/",
+            callback: 	"/",
+            success:	'/index',
+            fail:		'/'     
+        }
+    },
+	});
 });
 
 module.exports = router;
